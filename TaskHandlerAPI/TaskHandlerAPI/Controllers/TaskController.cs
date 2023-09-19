be00572df0996b0cd766aa61aa222fc894a23257
KevC <kevin.clerkin1@gmail.com>
2023-09-20 00:03:46 +0100

    Added API Request Methods

--- TaskHandlerAPI/TaskHandlerAPI/Controllers/TaskController.cs
+++ TaskHandlerAPI/TaskHandlerAPI/Controllers/TaskController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskHandlerAPI.Data;
 using TaskHandlerAPI.Models;
+
+
 
 namespace TaskHandlerAPI.Controllers
 {
@@ -8,14 +12,67 @@ namespace TaskHandlerAPI.Controllers
   [ApiController]
   public class TaskController : ControllerBase
   {
-    [HttpGet]
-    public async Task<ActionResult<List<TaskToDo>>> GetAllTasks()
+    private readonly DataContext _dataContext;
+
+    public TaskController(DataContext context)
     {
-      return new List<TaskToDo>
-      {
-        new TaskToDo { Id = 1, Text = "Laundry", Day = "Wednesday 13th May", Reminder = false}
-      };
+      _dataContext = context;
     }
-     
+
+
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TaskToDo>>> GetAllTasks()
+    {
+      var tasks = await _dataContext.TasksToDo.ToListAsync();
+      return Ok(tasks);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<IEnumerable<TaskToDo>>> CreateTask(TaskToDo task)
+    {
+      _dataContext.TasksToDo.Add(task);
+      await _dataContext.SaveChangesAsync();
+
+      return Ok(await _dataContext.TasksToDo.ToListAsync());
+
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<IEnumerable<TaskToDo>>> UpdateTask(int id, TaskToDo updateReminder)
+    {
+      var dbTask = await _dataContext.TasksToDo.FindAsync(id);
+
+      if (dbTask == null)
+      {
+        return BadRequest("Task not found!");
+      }
+
+      dbTask.Reminder = updateReminder.Reminder;
+
+      await _dataContext.SaveChangesAsync();
+
+      return Ok(await _dataContext.TasksToDo.ToListAsync());
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<IEnumerable<TaskToDo>>> DeleteTask(int id)
+    {
+      var dbTask = await _dataContext.TasksToDo.FindAsync(id);
+      if(dbTask == null)
+      {
+        return BadRequest("Task not found");
+      }
+
+      _dataContext.TasksToDo.Remove(dbTask);
+
+      await _dataContext.SaveChangesAsync();
+
+      return Ok(await _dataContext.TasksToDo.ToListAsync());
+
+    }
+
+
+
   }
 }
--- TaskHandlerAPI/TaskHandlerAPI/Data/DataContext.cs
+++ TaskHandlerAPI/TaskHandlerAPI/Data/DataContext.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using TaskHandlerAPI.Models;
 
+
 namespace TaskHandlerAPI.Data
 {
   public class DataContext : DbContext
-  {
+    {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
     public DbSet<TaskToDo> TasksToDo { get; set; }
